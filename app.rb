232c3d01fa055f588339abb09d7f19f33cf2118c
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-06-28 18:19:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/reversename/:name' do
-    "#{parms[:name].reverse}"
+    @name = params[:name]
+    "#{@name.reverse}"
   end
 
 end
